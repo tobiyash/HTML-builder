b5d46acd24f2340352ad9712f38d35648266c6b7
Anton <ta.sphinx@gmail.com>
2023-05-08 23:37:25 +0300

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const copyDirDeep = async ({ from, into }) => {
+  const toCopy = await fs.readdir(from, { withFileTypes: true });
+  for (const entry of toCopy) {
+    const srcpath = path.join(from, entry.name);
+    const destpath = path.join(into, entry.name);
+    const destfold = path.dirname(destpath);
+    if (entry.isDirectory()) {
+      await fs.mkdir(destpath, { recursive: true });
+      await copyDirDeep({ from: srcpath, into: destpath });
+    }
+    if (entry.isFile()) {
+      await fs.mkdir(destfold, { recursive: true });
+      await fs.copyFile(srcpath, destpath);
+    }
+  }
+};
+
+const COPY_FROM = path.join(__dirname, 'files');
+const COPY_INTO = path.join(__dirname, 'files-copy');
+
+copyDirDeep({
+  from: COPY_FROM,
+  into: COPY_INTO,
+});
